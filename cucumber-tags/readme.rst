031c5bef581b90d229b67f3f2cce5e879805120d
Soumitra <email@email.com>
2021-03-13 10:38:34 +0530

    updated for cucumber and junit

--- cucumber-tags/readme.rst
+++ cucumber-tags/readme.rst
@@ -1,1 +1,1 @@
-You can go through the tutorial https://www.roytuts.com/cucumber-tags-example/
+You can go through the tutorial https://roytuts.com/cucumber-tags-example/
--- cucumber-tags/src/test/java/cucumber/tags/CukeTagRunner.java
+++ cucumber-tags/src/test/java/cucumber/tags/CukeTagRunner.java
@@ -6,9 +6,9 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(strict = true, features = "classpath:cuke/features/cucumberTag.feature", glue = "cucumber.tags.steps", monochrome = true, plugin = {
-		"pretty", "html:target/cucumber", "json:target/Cucumber.json",
-		"junit:target/Cucumber.xml" }, tags = { "@SIT and not @Smoke" })
+@CucumberOptions(/*strict = true,*/features = "classpath:cuke/features/cucumberTag.feature", glue = "cucumber.tags.steps", monochrome = true, plugin = {
+		"pretty", /*"html:target/cucumber", "json:target/Cucumber.json",*/ "html:target/reports/cucumber-html-report.html", "json:target/reports/cucumber.json",
+		"junit:target/Cucumber.xml" }, tags = "@SIT and not @Smoke"/*tags = { "@SIT and not @Smoke" }*/)
 public class CukeTagRunner {
 
 }
